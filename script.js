ea4455314fa7af0c99275f7a589d63e36fa18e82
Y4HYA4 <87914378+Y4HYA4@users.noreply.github.com>
2022-06-28 22:00:13 +0200

    hm

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const descriptionElement = document.getElementById("description");
 const imageElement = document.getElementById("cat_image");
 const fetchBtn = document.getElementById("fetch");
 async function getFact() {
-	const res = await fetch("https://hmm.herokuapp.com/facts/random");
+	const res = await fetch("https://gist.github.com/Y4HYA4/d37185f25b88d67a7daad8aaf3207d4b");
 	return await res.json();
 }
 
